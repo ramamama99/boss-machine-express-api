58aab0a005ce7ca8a7eedaf0033a08d67afa2d0a
Rama_99 <ramaadityo@gmail.com>
2026-06-08 13:15:08 +0700

    Add main apiRouter and minions CRUD routes

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const app = express();
 module.exports = app;
 
 // Add middleware for handling CORS requests from index.html
-
+const cors = require('cors');
+app.use(cors());
 
 // Add middware for parsing request bodies here:
+const bodyParser = require('body-parser')
 
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api',apiRouter);
 
 
--- server/api.js
+++ server/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionsRouter = require('./minions');
+const ideasRouter = require('./ideas');
+const meetingRouter = require('./meetings');
+
+apiRouter.use('/minions',minionsRouter);
+apiRouter.use('/ideas',ideasRouter);
+apiRouter.use('/meetings',meetingRouter);
 
 
 module.exports = apiRouter;
